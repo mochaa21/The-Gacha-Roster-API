f03b217f25ea5651adaf30050447f48a6b37d7d6
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-08 20:33:24 +0700

    feat: the gacha roster API

--- main.py
+++ main.py
@@ -16,16 +16,33 @@ mock_roster_response = """
 }
 """
 
-# TUGAS 1: Buat @timer_decorator di sini (Jangan lupa tampung dan return hasilnya)
+def timer_decorator(func: Callable) -> Callable:
+    @functools.wraps(func)
+    def myinner(*args, **kwargs):
+        print("process...")
+        time.sleep(3)
+        hasil = func(*args, **kwargs)
+        print("success!")
+        time.sleep(1.5)
+        return hasil
+    return myinner
 
+@timer_decorator
+def get_top_characters(api_data: str) -> List[str]:
+    parsed_json = json.loads(api_data)
+    daftar_char = parsed_json["characters"]
+    def char_criteria(data: Dict) -> tuple:
+        name = data["name"]
+        element = data["element"]
+        power = int(data["power"])
+        return (-power, name, element)
 
-# TUGAS 2: Buat fungsi get_top_characters(api_data: str) -> List[str]
-# Wajib dipasangkan @timer_decorator di atasnya.
-# Perbaiki cara penulisan f-string lu menggunakan kutip tunggal di dalam kurung kurawal.
-
+    pro_results = sorted(daftar_char, key=char_criteria)
+    results = [f"{char["name"]} ({char["element"]}) - {char["power"]}" for char in pro_results]
+    return results
 
 # --- EKSEKUSI ---
-# print("=== GACHA ROSTER SORTING ===")
-# roster = get_top_characters(mock_roster_response)
-# for char in roster:
-#     print(char)
+print("=== GACHA ROSTER SORTING ===")
+roster = get_top_characters(mock_roster_response)
+for char in roster:
+    print(char)
